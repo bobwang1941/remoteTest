23626592eed9aa3098fa742b0faa7b9f386b6d38
bobwang1941 <1753335364@qq.com>
2017-03-05 14:30:03 +0800

    首次提交到github上

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,20 @@
+
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 
+ * @author : zqwang
+ *
+ * @date : 2017年2月27日
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ demo/src/main/java/com/example/TestDemo1.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController的意思就是controller里面的方法都以json格式输出，不用再写什么jackjson配置的了！
+@RestController
+public class TestDemo1 {
+
+	@RequestMapping("/hello")
+	public String testDemo(){
+		System.out.println("hello word");
+		String str = "这是测试git提交";
+		return "hello";
+	}
+}
+
