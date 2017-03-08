35715a6c8969be7e0d610e2b5bb3f0a69f3fb5f7
bobwang1941 <1753335364@qq.com>
2017-03-08 14:12:49 +0800

    测试

--- demo/src/main/java/com/example/TestDemo1.java
+++ demo/src/main/java/com/example/TestDemo1.java
@@ -1,19 +1,29 @@
 package com.example;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 //@RestController的意思就是controller里面的方法都以json格式输出，不用再写什么jackjson配置的了！
 @RestController
 public class TestDemo1 {
+    @Resource
+    private JdbcTemplate jdbcTemple;
 
 	@RequestMapping("/hello")
+	@ResponseBody
 	public String testDemo(){
-		System.out.println("hello word");
-		String str = "这是测试git提交";
-		String str2 = "分支修改";
-		String str3 ="分支合并主干";
-		return "哈哈哈";
+//		List<DeliveryStatus> list = testService.findAllBuildProject();
+	    List<Map<String, Object>> list = jdbcTemple.queryForList("select * from auto_build_status");
+	    
+		return list.toString();
 	}
 }
 
